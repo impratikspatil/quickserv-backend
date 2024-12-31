0b421300aebdb441eef2d6528a4fc1481edad8a8
pratikpatil <impratikspatil@gmail.com>
2024-12-31 14:56:47 +0530

    Created Basic setup for quickserv backend using spring boot

---
+++ src/main/java/com/example/kptech/quickserv/QuickservApplication.java
@@ -0,0 +1,13 @@
+package com.example.kptech.quickserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kptech/quickserv/QuickservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kptech.quickserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
