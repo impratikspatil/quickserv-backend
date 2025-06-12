fd980546e44acdf70a6a0c9e9b4b8d2424a12800
pratikpatil <impratikspatil@gmail.com>
2025-06-12 20:14:29 +0530

    add BASE_FRONTEND_URL

--- src/main/java/com/example/kptech/quickserv/util/Constants.java
+++ src/main/java/com/example/kptech/quickserv/util/Constants.java
@@ -4,6 +4,9 @@ public class Constants {
 
     public static final String IMAGE_UPLOAD_DIR = "uploads/";
     public static final String BASE_URL = "https://quickserv-backend.onrender.com/";
-    public static final String BASE_FRONTEND_URL="https://quickserv.vercel.app/";
+    public static final String BASE_FRONTEND_URL="https://quickserv.vercel.app";
+
+//    public static final String BASE_URL = "http://localhost:8080/";
+//    public static final String BASE_FRONTEND_URL="http://localhost:3000";
 
 }
